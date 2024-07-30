46a377c7f670cd9d10734040e25cbf5b3ef3c3fb
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-07-30 16:34:52 +0100

    Update dm_reg_dataset_atamborska.py

--- analysis/dm_reg_dataset_atamborska.py
+++ analysis/dm_reg_dataset_atamborska.py
@@ -16,15 +16,39 @@ dm_cod= codelist_from_csv(codelist_path/ "nhsd-primary-care-domain-refsets-dm_co
 )
 dmres_cod= codelist_from_csv(codelist_path/"nhsd-primary-care-domain-refsets-dm_codres.csv")
 
-#Create index date and vector for which patients were registered then
-index_date= "yyyy-mm-dd"
-has_registration = practice_registrations.for_patient_on(index_date).exists_for_patient()
+#Create index date 
+index_date= "2024-03-31"
+
+#Create a boolean vector for which patients were registered up until the index date
+registered = practice_registrations.for_patient_on(index_date).exists_for_patient()
+
+#Create a boolean vector to filter patients >17 at index date
+PAT_AGE = (patients.age_on(index_date) >= 17)
+
+#Create a boolean vector to select patients with appropriate an DM code
+
+DMLAT_DAT
+DMRES_DAT
 
 
-#Define the dataset (=cohort)
+
+
+#Define the dataset (=cohort) - using the rules
+
+##Rule 1
+###Latest diabetes diagnosis is not followed by a diabetes resolved code.
+###Have a diabetes diagnosis in the patient record up to and including the achievement date.
+
+dm_reg_r1= (dataset.dmres_dat < dataset.dmlat_dat) | (
+    dataset.dmlat_dat.is_not_null() & dataset.dmres_dat.is_null()
+
+##Rule 2
+### Reject patients passed to this rule who are aged under 17 years old on the achievement date. Select the remaining patients.
+#dm_reg_r2= dataset.age = T
 
 
 
+# Create the population
+dataset.define_population(dm_reg_r1)
 
-dataset.define_population(patients.date_of_birth.is_on_or_before("1999-12-31"))
-dataset.define_population(has_registration)
+
