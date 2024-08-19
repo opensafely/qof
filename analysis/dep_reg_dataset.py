6358e14e069c14bd1b3602255982a0ddc27cac00
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2024-08-19 13:06:35 +0000

    style: Applying suggested formatting changes from review

--- analysis/dep_reg_dataset.py
+++ analysis/dep_reg_dataset.py
@@ -8,8 +8,12 @@ from ehrql.tables.tpp import (
 )
 
 dataset = create_dataset()
-qs_start_date = '2024-04-01' # Quality Service Start Date
-min_date = '2006-04-01' # Have their latest first or new episode of depression on or after 1 st April 2006.
+# Quality Service Start Date
+qs_start_date = '2024-04-01' 
+# Fixed date used in DEPR1_REG and DEPCC01 logic.
+# Depression register: Patients aged at least 18 years old whose latest
+# unresolved episode of depression is since 1 st April 2006.
+min_date = '2006-04-01'
 
 # import codelists
 dep_codelist = codelist_from_csv(
@@ -24,38 +28,35 @@ has_registration = (practice_registrations
                     .for_patient_on(qs_start_date)
                     .exists_for_patient())
 
-'''
-Create disease variables
-- DEPR_DAT = Latest date of depression diagnosis
-- DEPRES_DAT = Latest depression resolved date
-- PAT_AGE = Age at quality service start date
-'''
-dataset.DEPR_DAT = ((clinical_events.
+# Create disease variables
+# - DEPR_DAT = Latest date of depression diagnosis
+# - DEPRES_DAT = Latest depression resolved date
+# - PAT_AGE = Age at quality service start date
+
+dataset.depr_dat = ((clinical_events.
                     where(clinical_events
                           .snomedct_code
                           .is_in(dep_codelist))
                           ).sort_by(clinical_events.date)
                           .last_for_patient()
                           .date)
-dataset.DEPRES_DAT = (clinical_events
+dataset.depres_dat = (clinical_events
                       .where(clinical_events
                              .snomedct_code
                              .is_in(depres_codelist))
                              .sort_by(clinical_events.date)
                              .last_for_patient()
                              .date)
-dataset.PAT_AGE = (patients
+dataset.pat_age = (patients
                    .age_on(qs_start_date))
 
-'''
-Creating rules
-1. If DEPR_DAT >= 01/04/2006
-    AND
-    If DEPRES_DAT = Null
-2. If PAT_AGE >= 18 years
-'''
-dep1_reg_r1 = ((dataset.DEPR_DAT.is_on_or_after(min_date))
-               & (dataset.DEPRES_DAT.is_null()))
-dep2_reg_r2 = dataset.PAT_AGE >= 18
+# --- Creating rules ---
+# Have their latest first or new episode of depression on or after 1 st April 2006.
+# Latest episode of depression is not followed by a depression resolved code.
+# Must be aged 18 or over on the achievement date
+
+dep1_reg_r1 = ((dataset.depr_dat.is_on_or_after(min_date))
+               & (dataset.depres_dat.is_null()))
+dep2_reg_r2 = dataset.pat_age >= 18
 
 dataset.define_population(has_registration & dep1_reg_r1 & dep2_reg_r2)
