05d2a33f62ffa792fe9d678ffc09a619fed7e929
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-08-20 11:47:36 +0000

    Add dataset test script

---
+++ analysis/test_dem_qof_measures_arina.py
@@ -0,0 +1,57 @@
+# Question - can we test the dataset developed via the measures framework?
+# Run this using the command: opensafely exec ehrql:v1 assure analysis test_dem_qof_measures_arina.py
+
+from datetime import date
+from dem_qof_measures_arina.py import dataset
+
+test_data = {
+    # Patient expected in the population
+    1: {
+        # Rule 1: Is registered  
+        "practice_registrations": [
+            {
+                "start_date": date(2009, 6, 7),
+                "end_date": NULL,
+            },
+        ],
+        # Rule 2: Has dementia
+        "clinical_events": [
+            {
+                "date": date(2015, 7, 8)
+                "snomedct_code": "429998004"
+            }
+        ]
+        "expected_in_population": True,
+    }
+    # Patient not expected the population: no dementia
+    2: {
+        # Rule 1: Is registered  
+        "practice_registrations": [
+            {
+                "start_date": date(2009, 6, 7),
+                "end_date": NULL,
+            },
+        ],
+        # Rule 2: No clinical events
+        "clinical_events": [{}],
+        "expected_in_population": False,
+    }
+    # Patient not expected in population, not registered
+    3:{
+        # Rule 1: Not registered during the interval
+        "practice_registrations": [
+            {
+                "start_date": date(2009, 6, 7),
+                "end_date": date(2014, 1, 1),
+            },
+        ],
+        # Rule 2: Has dementia
+        "clinical_events": [
+            {
+                "date": date(2015, 7, 8)
+                "snomedct_code": "429998004"
+            }
+        ]
+        "expected_in_population": False,
+    }
+}
