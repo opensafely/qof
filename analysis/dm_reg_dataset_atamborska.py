111012691c7c3b293fd1f0d276702a35bd180ab3
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-08-20 14:41:01 +0000

    Dataset test added

--- analysis/dm_reg_dataset_atamborska.py
+++ analysis/dm_reg_dataset_atamborska.py
@@ -1,66 +1,89 @@
-#Import functions that will be used for dataset definition
+# Import functions that will be used for dataset definition
 from ehrql import create_dataset
 from ehrql import codelist_from_csv
 from ehrql import Path
 
-#Import the tables of the variables which you need to define the dataset
+# Import the tables of the variables which you need to define the dataset
 from ehrql.tables.tpp import patients, practice_registrations, clinical_events
 
 ## could use from ehrql.tables.core or from ehrql.tables.emis
 
-#Create an empty dataset - we will be adding columns to this, based on which we will filter data
+# Create an empty dataset - we will be adding columns to this, based on which we will filter data
 dataset = create_dataset()
 
-#Create objects containing codelists - these will be used to define the dataset
-codelist_path= Path("codelists")
-dm_cod= codelist_from_csv(codelist_path/ "nhsd-primary-care-domain-refsets-dm_cod.csv", 
-                          column="code",)
-dmres_cod= codelist_from_csv(codelist_path/"nhsd-primary-care-domain-refsets-dmres_cod.csv", 
-                             column="code",)
+# Create objects containing codelists - these will be used to define the dataset
+codelist_path = Path("codelists")
+dm_cod = codelist_from_csv(
+    codelist_path / "nhsd-primary-care-domain-refsets-dm_cod.csv",
+    column="code",
+)
+dmres_cod = codelist_from_csv(
+    codelist_path / "nhsd-primary-care-domain-refsets-dmres_cod.csv",
+    column="code",
+)
 
 # Simpler:
-dm_cod= codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-dm_cod.csv",
-                          column="code",)
-dmres_cod= codelist_from_csv("codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv",
-                            column="code",)
+dm_cod = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv",
+    column="code",
+)
+dmres_cod = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv",
+    column="code",
+)
 
-#Create index date 
-index_date= "2024-03-31"
+# Create index date
+index_date = "2024-03-31"
 
-#Create a boolean vector for which patients were registered up until the index date
+# Create a boolean vector for which patients were registered up until the index date
 registered = practice_registrations.for_patient_on(index_date).exists_for_patient()
 
-#However, unless we add this information to the dataset, we won't be able to filter based on it, using dataset.define_population
+# However, unless we add this information to the dataset, we won't be able to filter based on it, using dataset.define_population
 ##So first, we need to add a column to the dataset which defines which patients are registered
-dataset.registered= practice_registrations.for_patient_on(index_date).exists_for_patient()
+dataset.registered = practice_registrations.for_patient_on(
+    index_date
+).exists_for_patient()
 
 # And only - then : Filter population to those registered - however, define_population can only be used once per script - so we will add it to the rules at the end
 ##dataset.define_population(registered)
 
-#Create a boolean vector to filter patients >17 at index date and add it to the dataset
-dataset.pat_age = (patients.age_on(index_date))
+# Create a boolean vector to filter patients >17 at index date and add it to the dataset
+dataset.pat_age = patients.age_on(index_date)
 
-#Create an object storing latest dates for Dx DM and Dx DM res for each patient
+# Create an object storing latest dates for Dx DM and Dx DM res for each patient
 
 ## Select the latest date with the diabetes code and add it to the dataset
-dataset.latest_dm= clinical_events.where(clinical_events.snomedct_code.is_in(dm_cod)).sort_by(clinical_events.date).last_for_patient().date
+dataset.latest_dm = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(dm_cod))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
 
 
 ## Select the latest date with the diabetes resolved code and add it to the dataset
-dataset.latest_dmres= clinical_events.where(clinical_events.snomedct_code.is_in(dmres_cod)).sort_by(clinical_events.date).last_for_patient().date
+dataset.latest_dmres = (
+    clinical_events.where(clinical_events.snomedct_code.is_in(dmres_cod))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
 
-#Define the dataset (=cohort) - using the rules
+# Define the dataset (=cohort) - using the rules
 
 ##Rule 1
 ###Latest diabetes diagnosis is not followed by a diabetes resolved code.
 ###Have a diabetes diagnosis in the patient record up to and including the achievement date.
 
-dm_reg_r1= (dataset.latest_dmres < index_date) & ((dataset.latest_dmres < dataset.latest_dm) | (dataset.latest_dm.is_not_null() & dataset.latest_dmres.is_null()))
+dm_reg_r1 = dataset.latest_dm.is_not_null() & (dataset.latest_dm < index_date) & (
+    (dataset.latest_dmres < dataset.latest_dm)
+    | dataset.latest_dmres.is_null()
+)
 
 ##Rule 2
 ### Reject patients passed to this rule who are aged under 17 years old on the achievement date. Select the remaining patients.
 
-dm_reg_r2= dataset.pat_age >= 17
+dm_reg_r2 = dataset.pat_age >= 17
 
 # Create the population
 dataset.define_population(registered & dm_reg_r1 & dm_reg_r2)
---
+++ analysis/test_dm_reg_dataset_atamborska.py
@@ -0,0 +1,40 @@
+# Set up
+from datetime import date
+from dm_reg_dataset_atamborska import dataset
+
+test_data = {
+    # Patient accepted in the dataset
+    1: {
+        "patients": {"date_of_birth": date(1992, 1, 1)},
+        "practice_registrations": [
+            {
+                # First registration
+                "start_date": date(1994, 1, 1),
+                "end_date": date(1999, 1, 1),
+            },
+            {
+                # Second registration
+                "start_date": date(1999, 1, 2),
+            },
+        ],
+        "clinical_events": [
+            # First clinical event
+            {
+                "snomedct_code": "313435000",
+                "date": date(2000, 4, 13),
+            },
+            # Second clinical event
+            {
+                "snomedct_code": "420270002",
+                "date": date(2004, 1, 17),
+            },
+        ],
+        "expected_in_population": True,
+        "expected_columns": {
+            "pat_age": 32,
+            "registered": True,
+            "latest_dm": date(2004, 1, 17),
+            "latest_dm_res": None,
+        },
+    }
+}
